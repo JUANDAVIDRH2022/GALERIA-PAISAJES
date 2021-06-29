53d26dbdb9661e40d335ac1bc4751f5dedabb291
JUANDAVIDRH2022 <86641738+JUANDAVIDRH2022@users.noreply.github.com>
2021-06-29 00:31:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -74,3 +74,19 @@ Vue.component('card', {
 
 const app = new Vue({
   el: '#app' });
+
+const hamburguer = document.querySelector('.hamburguer')
+const menu = document.querySelector('.menu-navegacion')
+
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e =>{
+    if(menu.classList.contains('spread') 
+        && e.target != menu && e.target != hamburguer){
+        console.log('cerrar')
+        menu.classList.toggle("spread")
+    }
+})
